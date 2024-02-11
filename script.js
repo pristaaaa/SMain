2739942277965f2fc46bb38d75621549790d5dc0
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-11 12:51:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function getMPDFile() {
+  return "https://linear011-gb-dash1-prd-cf.cdn.skycdp.com/016a/Content/DASH_003_hd/Live/channel(sportsmainevent50)/manifest_hd.mpd";
+}
+
+function getClearKey() {
+  return "a5dcfa9627379c6928567092b6e5ce40";
+}
+
+function getClearKeyId() {
+  return "00034d0b4c47a219e93d880b4610d197";
+}
